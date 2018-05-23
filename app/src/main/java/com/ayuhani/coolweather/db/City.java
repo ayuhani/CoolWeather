265bda8d788af6d5fa95f7e583d204997572a001
ayuhani <1132529645@qq.com>
2018-05-23 10:46:35 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/ayuhani/coolweather/db/City.java
+++ app/src/main/java/com/ayuhani/coolweather/db/City.java
@@ -1,8 +1,47 @@
 package com.ayuhani.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by wang on 2018/5/23.
  */
 
-public class City {
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName == null ? "" : cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 }
--- app/src/main/java/com/ayuhani/coolweather/db/Province.java
+++ app/src/main/java/com/ayuhani/coolweather/db/Province.java
@@ -1,8 +1,38 @@
 package com.ayuhani.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by wang on 2018/5/23.
  */
 
-public class Province {
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName == null ? "" : provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
 }
